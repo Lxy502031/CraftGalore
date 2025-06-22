11a70169b880ee00bcc25c6a8279473e531a6c67
xinyang22 <limxinyang12345@gmail.com>
2025-06-22 17:46:37 +0800

    updated site

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  // Tell Eleventy to copy these files/folders to the output without processing
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("script.js");
+  eleventyConfig.addPassthroughCopy("product/media");  // your images folder
+
+  eleventyConfig.addCollection("products", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./product/*.md");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
